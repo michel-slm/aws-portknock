84b061405af105be6240d0a26aa79c9731584cf6
Michel Alexandre Salim <michel@michel-slm.name>
2015-12-25 09:31:04 +0700

    Refactor dependencies to use requirements.txt Add boto3 dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
+from pip.req import parse_requirements
 from setuptools import setup
 
 setup(
     name='aws-portknock',
     version='0.1',
     py_modules=['aws_portknock'],
-    install_requires=[
-        'Click',
-    ],
+    install_requires=[str(r.req)
+                      for r in parse_requirements('requirements.txt')],
     entry_points='''
         [console_scripts]
         aws-portknock=aws_portknock:cli
