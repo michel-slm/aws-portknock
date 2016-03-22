1ec4dfc0c8a2bc3cb5432881dccf4f711b1bd7aa
Michel Alexandre Salim <michel@michel-slm.name>
2016-03-22 22:51:58 +0700

    Fix setup.py for pip 6.0+ compatibility by adding session
    cf https://stackoverflow.com/a/32268735

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from pip.download import PipSession
 from pip.req import parse_requirements
 from setuptools import setup
 
@@ -6,7 +7,9 @@ setup(
     version='0.1',
     py_modules=['aws_portknock'],
     install_requires=[str(r.req)
-                      for r in parse_requirements('requirements.txt')],
+                      for r in parse_requirements(
+                        'requirements.txt',
+                        session=PipSession())],
     entry_points='''
         [console_scripts]
         aws-portknock=aws_portknock:cli
