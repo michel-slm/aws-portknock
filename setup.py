f787d816a47b0f028374f78e3bf860968a95c4f7
Michel Alexandre Salim <michel@michel-slm.name>
2016-04-09 21:22:56 +0700

    Add descriptions for PyPI
    cf http://peterdowns.com/posts/first-time-with-pypi.html

--- setup.py
+++ setup.py
@@ -6,6 +6,13 @@ setup(
     name='aws-portknock',
     version='0.1',
     py_modules=['aws_portknock'],
+    description='Port knocking for AWS security groups',
+    author='Michel Alexandre Salim',
+    author_email='michel@michel-slm.name',
+    url='https://github.com/michel-slm/aws-portknock',
+    download_url='https://github.com/michel-slm/aws-portknock/tarball/0.1',
+    keywords=['aws'],
+    classifiers=[],
     install_requires=[str(r.req)
                       for r in parse_requirements(
                         'requirements.txt',
